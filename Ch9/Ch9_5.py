cefe47ec359a7ab3a764e8d9d8908bfffc0596c4
minshikkim <kmss69052@naver.com>
2020-11-07 20:58:41 +0900

    add matrix multification

--- Ch9/Ch9_5.py
+++ Ch9/Ch9_5.py
@@ -1,32 +1,15 @@
 matrix_a = [[1, 2, 3], [4, 5, 6]]
 matrix_b = [[1, 1], [2, 1], [1, 3]]
 matrix_c = [[1, 4, 2, 6], [2, 5, 7, 8]]
-# 첫번 쨰 행렬의 행을 뽑는다.
-# 두번 쨰 행렬의 열을 뽑는다.
-# result = [[sum(a*b) for a, b in zip(row_a, column_b) for column_b in zip(*matrix_b)] for row_a in matrix_a]
-# print(result)
+matrix_d = [[1, 2], [3, 4], [5, 6], [9, 1]]
 
-# for row_a in matrix_a:
-#     for column_b in zip(*matrix_b):
-#         print(row_a, column_b)
-#         for a, b in zip(row_a, column_b):
-#             print(a, b, a * b)
+def matrix_mul(*args):
+    lists = [*args]
+    if len(args) == 1:
+        return print(*lists)
+    result = [[sum(a * b for a, b in zip(row_a, column_b)) for column_b in zip(*args[1])] for row_a in args[0]]
+    lists.pop(1)
+    lists[0] = result
+    matrix_mul(*lists)
 
-result = []
-for row_a in matrix_a:
-    row_v = []
-    for column_b in zip(*matrix_b):
-        print(row_a, column_b)
-        dot_product = 0
-        for r_a, c_b in zip(row_a, column_b):
-            dot_product += r_a * c_b
-        row_v.append(dot_product)
-    result.append(row_v)
-
-print(result)
-
-
-
-
-
-
+matrix_mul(matrix_a, matrix_b, matrix_c, matrix_d)
